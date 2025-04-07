ad8e6388c218932fd6857eb37ffdd1832d8ba6f7
Cyril <cyrilboidoppc@gmail.com>
2025-04-07 18:16:07 +0200

    Datepicker

--- scripts/demandeDetails.js
+++ scripts/demandeDetails.js
@@ -1,13 +1,16 @@
 import { initializeHeader } from "/scripts/components/header.js";
 import { initializeTestimonialBadges } from "/scripts/components/demandeTestimonials.js";
 import { initializeCountryCodeSelector } from "/scripts/components/countryCodeSelector.js";
+import { initializeBirthdateFields } from "/scripts/components/datePicker.js";
 
 // Initialize everything when DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
   // Initialize header
   initializeHeader();
-  // Initialize Testimonial Badges
-  initializeTestimonialBadges();
+  // Initialize Date Picker
+  initializeBirthdateFields();
   // Initialize Country Code Selector
   initializeCountryCodeSelector();
+  // Initialize Testimonial Badges
+  initializeTestimonialBadges();
 });
